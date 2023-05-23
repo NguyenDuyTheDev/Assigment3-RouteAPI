43f2f1ea1fd3b3777213741f82bc23965cea8b4d
NguyenDuyTheDev <125572099+NguyenDuyTheDev@users.noreply.github.com>
2023-05-23 21:44:27 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+const PORT = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   /*await app.listen(process.env.PORT);*/
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+  .setTitle('BusApp API')
+  .setDescription('API dành cho ứng dụng BusApp. Được thiết kế bởi nhóm 1 dành cho bài tập lớn số 3 của môn Phát triển ứng dụng cho thiết bị di động.')
+  .setVersion('1.0')
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(PORT);
 }
 bootstrap();
